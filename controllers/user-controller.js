646c4be4d15bdf22806bc2035146ce113be0bdd0
David Binyatov <davidbphotography101@gmail.com>
2022-04-10 05:42:19 -0400

    deleteuser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -59,6 +59,19 @@ const userController = {
       .catch((err) => res.status(400).json(err));
   },
 
+  //  Remove a user's associated thoughts when deleted.
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
 
 
 module.exports = userController;
