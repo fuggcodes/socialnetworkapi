aa71bab597db1335efb38899318e7f160dae8b84
David Binyatov <davidbphotography101@gmail.com>
2022-04-10 05:41:48 -0400

    create user and update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -36,5 +36,29 @@ const userController = {
         res.status(400).json(err);
       });
   },
+  // create User
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+
 
 module.exports = userController;
