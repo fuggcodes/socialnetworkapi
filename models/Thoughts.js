1513d46ef5adb510abca7050b8626cb9a244bb89
David Binyatov <davidbphotography101@gmail.com>
2022-04-10 05:44:44 -0400

    reactionschema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -31,40 +31,6 @@ const ReactionSchema = new Schema(
   }
 );
 
-const ThoughtsSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: "Thought is required",
-      validate: [
-        ({ length }) => length < 280,
-        "Thought should be less than 280 characters",
-      ],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
-    username: {
-      type: String,
-      required: "Username is required",
-    },
-    reactions: [ReactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
 
-ThoughtsSchema.virtual("reactionCount").get(function () {
-  return this.reactions.length;
-});
-
-const Thoughts = model("Thoughts", ThoughtsSchema);
 
 module.exports = Thoughts;
