1c3124bc3530d7a0b2e8e526cdf881a157aabaca
David Binyatov <davidbphotography101@gmail.com>
2022-04-10 05:16:42 -0400

    new date

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -16,3 +16,56 @@ const addDateSuffix = (date) => {
 
   return dateStr;
 };
+
+// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+module.exports = (
+  timestamp,
+  { monthLength = "short", dateSuffix = true } = {}
+) => {
+  let months;
+
+  if (monthLength === "short") {
+    months = {
+      0: "Jan",
+      1: "Feb",
+      2: "Mar",
+      3: "Apr",
+      4: "May",
+      5: "Jun",
+      6: "Jul",
+      7: "Aug",
+      8: "Sep",
+      9: "Oct",
+      10: "Nov",
+      11: "Dec",
+    };
+  } else {
+    months = {
+      0: "January",
+      1: "February",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "June",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December",
+    };
+  }
+  const dateObj = new Date(timestamp);
+  const formattedMonth = months[dateObj.getMonth()];
+
+  let dayOfMonth;
+
+  if (dateSuffix) {
+    dayOfMonth = addDateSuffix(dateObj.getDate());
+  } else {
+    dayOfMonth = dateObj.getDate();
+  }
+
+  const year = dateObj.getFullYear();
+
+  let hour;
