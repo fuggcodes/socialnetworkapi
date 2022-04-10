bbb705d55236eed022a6f3db151e7e7e2f189f65
David Binyatov <davidbphotography101@gmail.com>
2022-04-10 05:36:02 -0400

    gethoughtsbyid

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -14,3 +14,21 @@ const thoughtsController = {
       });
   },
 
+
+  // get one thought by ID
+
+  getThoughtsById({ params }, res) {
+    Thoughts.findOne({ _id: params.id })
+      .select("-__v")
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: "No thought found with this ID." });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
